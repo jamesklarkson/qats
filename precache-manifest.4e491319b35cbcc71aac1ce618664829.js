8d060b3dbe5c189df1039a23fccec5339b63e55c
jamesklarkson <jamesklarkson@users.noreply.github.com>
2024-02-07 12:40:07 +0000

    deploy: 7a345665fbd5a0a20fd3424a284cc203ecc1b98a

---
+++ precache-manifest.4e491319b35cbcc71aac1ce618664829.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72e7e9288e4618322ab1378492cd4261",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51ed67b59d968644ee61",
+    "url": "/static/css/main.89b51140.chunk.css"
+  },
+  {
+    "revision": "94da162608abf8713e81",
+    "url": "/static/js/2.9b61cf2d.chunk.js"
+  },
+  {
+    "revision": "d8788d44faf507165f2271943b1644f8",
+    "url": "/static/js/2.9b61cf2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51ed67b59d968644ee61",
+    "url": "/static/js/main.3bfad627.chunk.js"
+  },
+  {
+    "revision": "3b0fa74f1082d0ce647d",
+    "url": "/static/js/runtime-main.75b8a3af.js"
+  },
+  {
+    "revision": "2a47a29ceb33c966c8d79f8d5a5ea448",
+    "url": "/static/media/Poppins-Light.2a47a29c.ttf"
+  }
+]);
